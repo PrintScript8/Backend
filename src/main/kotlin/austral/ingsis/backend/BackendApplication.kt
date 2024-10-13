f33be5dc54f3bcf5e550f4ab6769ff64ba6f0266
HiluLagos <128090306+HiluLagos@users.noreply.github.com>
2024-10-13 17:12:14 -0300

    Working authentication Variables are stored in .env and request is made on terminal

--- src/main/kotlin/austral/ingsis/backend/BackendApplication.kt
+++ src/main/kotlin/austral/ingsis/backend/BackendApplication.kt
@@ -1,5 +1,6 @@
 package austral.ingsis.backend
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,15 @@ import org.springframework.boot.runApplication
 class BackendApplication
 
 fun main(args: Array<String>) {
-    runApplication<BackendApplication>(args.toString())
+    val dotenv = Dotenv.load()
+
+    // Load environment variables into the system properties
+    System.setProperty("AUTH0_AUDIENCE", dotenv["AUTH0_AUDIENCE"])
+    System.setProperty("AUTH_SERVER_URI", dotenv["AUTH_SERVER_URI"])
+    System.setProperty("AUTH_CLIENT_ID", dotenv["AUTH_CLIENT_ID"])
+    System.setProperty("AUTH_CLIENT_SECRET", dotenv["AUTH_CLIENT_SECRET"])
+    System.setProperty("AUTH0_USER_EMAIL", dotenv["AUTH0_USER_EMAIL"])
+    System.setProperty("AUTH0_USER_PASSWORD", dotenv["AUTH0_USER_PASSWORD"])
+
+    runApplication<BackendApplication>(*args)
 }
--- src/main/kotlin/austral/ingsis/backend/service/processing/FileProcessingService.kt
+++ src/main/kotlin/austral/ingsis/backend/service/processing/FileProcessingService.kt
@@ -9,7 +9,12 @@ class FileProcessingService {
     val processorFactory = ProcessorFactory()
 
     fun processFile(file: MultipartFile) {
-        // obtener filetype
+        val originalFilename = file.originalFilename
+        val extension = originalFilename?.substringAfterLast('.', "")
+        val processor: FileProcessor? = extension?.let { processorFactory.getProcessor(it.lowercase()) }
+        if (processor == null){
+            // error porque no se sabe procesar
+        }
         // usar filetype en el factory "getProcessor(filetype)"
         // todo: implement method
     }
